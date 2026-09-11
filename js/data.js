7a1fe8299e86d271446d964eafc994ade44882ff
Arijeet Roy <34400226+arijeetsdiary@users.noreply.github.com>
2026-09-11 12:05:12 +1000

    Update data.js

--- js/data.js
+++ js/data.js
@@ -93,7 +93,8 @@ const ALBUMS = [
   },
   {
     title: "Perth during Easter 2026",
-    description: "The first time you met in person, followed by a long drive down to Busselton.",
+    dates: "1-3 April 2026"
+    description: "The first time we met alone, followed by a long drive down to Busselton.",
     cover: "assets/images/Album_2_Perth_during_Easter_2026/Cover_Perth_during_Easter_2026.jpg",
     photos: [
       { image: "", caption: "Add a caption for this photo." },
